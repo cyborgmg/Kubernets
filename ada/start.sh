a97036d91058f41ca1c91fcf73a4dc29e90ff998
cyborgmg <cyborgmg@gmail.com>
2020-11-05 11:58:03 -0300

    update-youtube

--- ada/start.sh
+++ ada/start.sh
@@ -13,15 +13,18 @@ kubectl create -f postgres/deployment.yaml
 kubectl create -f postgres/service.yaml 
 
 sleep 30
+kubectl get pods
 
 kubectl create -f java/configuration.yaml
 kubectl create -f java/deployment.yaml
 kubectl create -f java/services.yaml 
 kubectl delete hpa ada-api
-kubectl autoscale deployment ada-api --cpu-percent=50 --min=1 --max=3
+kubectl autoscale deployment ada-api --cpu-percent=50 --min=1 --max=2
 minikube service ada-api --url
 
+
 sleep 2
+kubectl get hpa
 
 kubectl create -f nginx/configuration.yaml
 kubectl create -f nginx/deployment.yaml
